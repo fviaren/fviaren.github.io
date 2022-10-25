332e8063ccba19a4de78d0aff3e9c440efd586c9
Felicitas Viarenghi <felicitas.viarenghi@wolt.com>
2022-10-25 20:52:01 +0200

    Change colors

--- src/colors.css.ts
+++ src/colors.css.ts
@@ -1,8 +1,9 @@
 export const colors = {
-  background: "#ffd500",
-  text: "#005bbb",
-  primary: "#005bbb",
-  muted: "#f5cc00",
-  active: "#004287",
+  background: "#e6dfcc",
+  text: "#412517",
+  primary: "#412517",
+  secondary: "#a75f3b",
+  muted: "#50b5a7",
+  active: "#1b0a02",
   black: "#000",
 }
