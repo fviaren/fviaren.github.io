a1ca5a1e5c40ef6d28f2e73f46ce89d25f778d5f
fviaren <62218282+fviaren@users.noreply.github.com>
2022-10-25 21:07:23 +0000

    Deploying to docs from @ fviaren/fviaren.github.io@100f21ef350861e7609f53183839799c21e9c892 🚀

--- component---src-pages-page-slug-js-09c15aeb54a0145bcfc5.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_contentful_homepage=self.webpackChunkgatsby_starter_contentful_homepage||[]).push([[686],{7374:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var a=n(7294),r=n(5889),l=n(1038);function u(e){var t=e.data.page;return a.createElement(r.Z,t,a.createElement(l.xu,{paddingY:5},a.createElement(l.W2,{width:"narrow"},a.createElement(l.X6,{as:"h1"},t.title),a.createElement("div",{dangerouslySetInnerHTML:{__html:t.html}}))))}}}]);
-//# sourceMappingURL=component---src-pages-page-slug-js-09c15aeb54a0145bcfc5.js.map
