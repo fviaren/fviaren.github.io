baf5fdbb2a01b117f4cdf4b13d24c27f5b745df2
Felicitas Viarenghi <felicitas.viarenghi@wolt.com>
2022-10-25 21:31:17 +0200

    Change font

--- src/theme.css.ts
+++ src/theme.css.ts
@@ -46,9 +46,9 @@ const fontWeights = {
 }
 
 const fonts = {
-  text: '"DM Sans", sans-serif',
-  heading: '"DM Sans", sans-serif',
-  mono: "DM Mono, Menlo, monospace",
+  text: '"Quicksand", sans-serif',
+  heading: '"Quicksand", sans-serif',
+  mono: "Quicksand, Menlo, monospace",
 }
 
 const lineHeights = {
